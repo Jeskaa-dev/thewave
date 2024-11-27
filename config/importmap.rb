2a87f880c5315a60531eaef3cbafce84f3e28401
Vincent Lelong <vincent.lelong1995@gmail.com>
2024-11-27 12:55:48 +0100

    Tailwind Set Up

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,5 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js", preload:true
 pin "@popperjs/core", to: "popper.js", preload:true
+pin "flowbite", to: "https://cdn.jsdelivr.net/npm/flowbite@2.5.2/dist/flowbite.turbo.min.js"
