48bad0966610e2c1c5e334057d9998d62031f64a
Vincent Lelong <vincent.lelong1995@gmail.com>
2024-11-29 16:38:28 +0100

    new user show

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
   resources :users do
     resources :skills, only: %i[index]
     resources :user_skills, only: %i[index]
+    resources :training_plans, only: %i[show]
   end
 
   resources :training_plans do
-    resources :resources, only: %i[index]
-    resources :skills, only: %i[index]
+    resources :resources, only: %i[show]
+
   end
 end
