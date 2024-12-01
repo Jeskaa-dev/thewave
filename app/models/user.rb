7a9ab53b0449e25c913b7a910c166448598d8bf0
Vincent Lelong <vincent.lelong1995@gmail.com>
2024-12-01 15:00:21 +0100

    Modification constante GITHUB_PATH

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
     # token = self.github_token
     token = ENV['LELONG_TOKEN']
     username = "Vincent-lelong"
-   " https://github.com/Vincent-lelong/fullstack-challenges/tree/master/01-Ruby/01-Programming-basics/01-Buddy"
+
     GITHUB_PATHS.each do |repo, data|
       path = data[:path]
       langage = data[:langage]
@@ -85,6 +85,44 @@ class User < ApplicationRecord
     @commit_status
   end
 
+
+  def user_commits
+    @commit_status = {}
+    # token = self.github_token
+    token = ENV['LELONG_TOKEN']
+    username = "Vincent-lelong"
+
+    GITHUB_PATHS.each do |repo, data|
+      path = data[:path]
+      langage = data[:langage]
+      optional = data[:Optional] == "true"
+      block = data[:block]
+      category = data[:category]
+      name = data[:name]
+      base_url = "https://api.github.com/repos/#{username}#{path}#{username}"
+      uri = URI(base_url)
+      puts uri
+
+      request = Net::HTTP::Get.new(uri)
+      request["Accept"] = "application/vnd.github+json"
+      request["Authorization"] = "Bearer #{token}"
+      request["X-GitHub-Api-Version"] = "2022-11-28"
+
+      response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+        http.request(request)
+
+      end
+
+      if response.code.to_i == 200
+        commits = JSON.parse(response.body)
+        @commit_status[repo] = { done: commits.any?, langage: langage, optional: optional, name: name, block: block, category: category }
+      else
+        Rails.logger.error("Failed to fetch commits for repo #{repo}: #{response.body}")
+        @commit_status[repo] = { done: false, langage: langage, optional: optional }
+      end
+      @commit_status
+    end
+  end
   private
 
   def create_training_plan
