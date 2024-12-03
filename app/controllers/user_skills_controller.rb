0100f489fabcae63565ee49d3b9d3ca8bb530677
Vincent Lelong <vincent.lelong1995@gmail.com>
2024-12-03 14:57:20 +0100

    mise a jour de la user-skill index

--- app/controllers/user_skills_controller.rb
+++ app/controllers/user_skills_controller.rb
@@ -5,5 +5,7 @@ class UserSkillsController < ApplicationController
     authorize @user_skills
     @user = current_user
     @commit_status = @user.commit_status
+    @top_user_skills = UserSkill.order(rating: :desc).limit(3)
+    @bottom_user_skills = UserSkill.order(rating: :asc).limit(3)
   end
 end
