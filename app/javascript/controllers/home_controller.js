7783eb47e97c48589b44658b83430deed8000ae1
Jerome Anciaux <jeskaa@hotmail.fr>
2024-12-04 16:45:55 +0100

    fin home page

--- app/javascript/controllers/home_controller.js
+++ app/javascript/controllers/home_controller.js
@@ -3,9 +3,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const images = [
     'https://res.cloudinary.com/dxqaj3mdo/image/upload/v1733318898/w5cep9i8saecicddxv65.png',
     'https://res.cloudinary.com/dxqaj3mdo/image/upload/v1733318898/drppcbyvo8t3fdw3wqpv.png',
+    'https://res.cloudinary.com/dxqaj3mdo/image/upload/v1733318897/psiuuz8akjveew2nyefz.png',
     'https://res.cloudinary.com/dxqaj3mdo/image/upload/v1733318898/kllkx6lax4q3m8uptkeo.png',
-    'https://res.cloudinary.com/dxqaj3mdo/image/upload/v1733318898/drppcbyvo8t3fdw3wqpv.png',
-    'https://res.cloudinary.com/dxqaj3mdo/image/upload/v1733318898/xykbkgqtyllaqgwfkky6.png',
+    'https://res.cloudinary.com/dxqaj3mdo/image/upload/v1733325154/vvxf1uedxvulgjkv8tts.png',
   ];
   const imageElement = document.getElementById('current-image');
   let currentParagraph = 0;
