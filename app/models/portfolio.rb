67caf49e2faf75b4b13e3aceb9464eb6085fa59b
Vincent Lelong <vincent.lelong1995@gmail.com>
2024-12-05 14:16:01 +0100

    création du controller stimulus step

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -6,7 +6,9 @@ class Portfolio < ApplicationRecord
     steps = [
       step_1_github_url, step_1_site_url,
       step_2_github_url, step_2_site_url,
-      step_3_github_url, step_3_site_url
+      step_3_github_url, step_3_site_url,
+      step_4_github_url, step_4_site_url,
+      step_5_github_url, step_5_site_url
     ]
 
     completed_steps = steps.compact.reject(&:blank?).count
