789c4011a79394b453be0bd73d8930a9459f973d
Jeskaa-dev <jeskaa@hotmail.fr>
2024-12-05 15:51:42 +0100

    delete flowbite from tailwind (#97)
    * for heroku
    
    * debugg
    
    * maj tailwind.css
    
    * skip page log-in
    
    * withou node modules
    
    * flowbite comment state
    
    * flowbite comment state
    
    * flowbite comment state
    
    ---------
    
    Co-authored-by: Julie Veran <julieveran31@gmail.com>

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
     './app/views/**/*.{erb,haml,html,slim}',
-    './node_modules/flowbite/**/*.js'
+    // './node_modules/flowbite/**/*.js'
   ],
   theme: {
     colors: {
@@ -28,6 +28,5 @@ module.exports = {
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
     require('@tailwindcss/container-queries'),
-    require('flowbite/plugin'),
   ]
 }
