67f0d415a57959576b9ba5bb8ac8df8c2f801731
Julie Veran <julieveran31@gmail.com>
2024-12-05 11:34:10 +0100

    problème chatbot - too many request

--- app/controllers/training_plans_controller.rb
+++ app/controllers/training_plans_controller.rb
@@ -12,13 +12,6 @@ class TrainingPlansController < ApplicationController
     @resources = Resource.where(id: @completions.map(&:resource_id))
     @skills = Skill.where(id: @user_skills.map(&:skill_id))
     authorize @training_plan
-
-    # client = OpenAI::Client.new
-    # chatgpt_response = client.chat(parameters: {
-    #   model: "gpt-4o-mini",
-    #   messages: [{ role: 'user', content: 'Give me more information in web development, without any of your own answer.' }]
-    # })
-    # @content = chatgpt_response['choices'][0]['message']['content']
   end
 
   # def edit
